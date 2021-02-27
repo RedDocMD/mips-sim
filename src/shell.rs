c55a0161b9c9e1543e792f0094ce84e9f8f7c8db
Deep Majumder <deep.majumder2019@gmail.com>
2021-02-28 00:16:15 +0530

    MemRegion write u32

--- src/shell.rs
+++ src/shell.rs
@@ -27,7 +27,7 @@ impl MemRegion {
     }
 
     fn read_32(&self, address: usize) -> Option<u32> {
-        if self.contains_address(address) {
+        if !self.contains_address(address) {
             return None;
         } else {
             let offset = address - self.start;
@@ -36,6 +36,19 @@ impl MemRegion {
             let byte1 = self.mem[offset + 1] as u32;
             let byte0 = self.mem[offset] as u32;
             return Some((byte3 << 24) | (byte2 << 16) | (byte1 << 8) | byte0);
+        }
+    }
+
+    fn write_32(&mut self, address: usize, value: u32) -> bool {
+        if !self.contains_address(address) {
+            return false;
+        } else {
+            let offset = address - self.start;
+            self.mem[offset + 3] = (value >> 24) as u8;
+            self.mem[offset + 2] = (value >> 16) as u8;
+            self.mem[offset + 1] = (value >> 8) as u8;
+            self.mem[offset] = value as u8;
+            return true;
         }
     }
 }
